018b08b325e934322f11d0a7523f06c4f700f06b
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-05-31 18:46:41 +0300

    v.6.1.3 (+ start task-3)

--- js/task-3.js
+++ js/task-3.js
@@ -12,3 +12,28 @@ class User {
 const allName = new User(`Alice`, 30);
 console.log(allName);
 console.log(allName.showName());
+
+
+
+class StringBuilder {
+    #value;
+    constructor (initialValue){
+        this.value = initialValue;
+    }
+
+    getValue() {
+        return this.value;
+    }
+
+    
+}
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
